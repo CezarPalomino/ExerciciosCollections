2056a15f84dec541429602e6b3f7aa37027459d3
Cezar Palomino <cezarprazec@hmail.com>
2021-11-18 21:40:10 -0300

    Envio dos Exercícios

--- Estoque.java
+++ Estoque.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 
 public class Estoque {
 	public static void main(String[] args) {
+		String nome = null;
+		String lote = null;
+		int quantidade = 0;
 		
 Scanner read = new Scanner(System.in);
 		
@@ -17,19 +20,32 @@ Scanner read = new Scanner(System.in);
 		
 		for(int i = 0; i < quantProduto; i++) {
 			System.out.println("Digite o nome do produto: ");
-			String nome = read.next();
+			nome = read.next();
 			read.nextLine();
 			System.out.println("Digite o lote do produto:");
-			String lote = read.next();
+			lote = read.next();
 			read.nextLine();
 			System.out.println("Digite a quantidade do produto:");
-			int quantidade = read.nextInt();
+			quantidade = read.nextInt();
 			
 			produto.add(new Produto(nome, lote, quantidade));
 		}
 		
 		for(Produto al : produto) {
 		System.out.println(al);
+		}
+		
+		System.out.println();
+		
+		produto.remove(0);
+		
+		System.out.println("Atualizando...");
+		System.out.println();
+		
+		produto.add(new Produto("Xcaixa", "585", 3));
+		
+		for(Produto al : produto) {
+			System.out.println(al);
 		}
 		
 		read.close();
